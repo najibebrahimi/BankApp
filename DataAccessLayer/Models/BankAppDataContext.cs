98d9286bd45a286e0379995862305f4b4ea5b8bb
najib ebrahimi <najib.ebrahimi144@gmail.com>
2025-04-22 14:16:55 +0200

    Jag har ändrat DataContext

--- DataAccessLayer/Models/BankAppDataContext.cs
+++ DataAccessLayer/Models/BankAppDataContext.cs
@@ -34,7 +34,7 @@ public partial class BankAppDataContext : IdentityDbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-54HBLFU;Database=BankAppData;Trusted_Connection=True;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=BankAppData;Trusted_Connection=True;MultipleActiveResultSets=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
